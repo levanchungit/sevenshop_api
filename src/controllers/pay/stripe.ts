99efe2f7a26b6c3e28d2fdb398fbcec8f56be545
levanchung.it <levanchung.it@gmail.com>
2023-04-05 16:45:26 +0700

    fix api stripe

--- src/controllers/pay/stripe.ts
+++ src/controllers/pay/stripe.ts
@@ -1,19 +1,30 @@
 import { Request, Response } from "express";
+import User from "models/user";
+import { getIdFromReq } from "utils/token";
 const stripe = require("stripe")(process.env.SECRET_KEY);
 
 const checkoutStripe = async (req: Request, res: Response) => {
   try {
+    const user = await User.findById(getIdFromReq(req));
+    if (!user) {
+      return res.sendStatus(403);
+    }
+    const customer = await stripe.customers.create({
+      id: user._id,
+      email: user.email,
+      name: user.full_name,
+      phone: user.phone,
+    });
     const paymentIntent = await stripe.paymentIntents.create({
       amount: req.body.amount,
       currency: "usd",
-      automatic_payment_methods: {
-        enabled: true,
-      },
+      customer: customer.id,
+      payment_method_types: ["card"],
     });
 
     res.json({ paymentIntent: paymentIntent.client_secret });
-  } catch (e) {
-    res.sendStatus(500);
+  } catch (e: any) {
+    res.status(500).json({ message: e.message });
   }
 };
 
