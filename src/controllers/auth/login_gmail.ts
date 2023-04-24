e5e790283f702989414fb5f279cd81eabda2a42e
levanchung.it <86552382+levanchung0808@users.noreply.github.com>
2023-04-24 21:10:14 +0700

    fix api login gmail

--- src/controllers/auth/login_gmail.ts
+++ src/controllers/auth/login_gmail.ts
@@ -5,45 +5,48 @@ import { getNow } from "utils/common";
 import { tokenGen } from "utils/token";
 
 const loginGmail = async (req: Request, res: Response) => {
-  const { email, avatar, full_name }: IUser = req.body;
+  try {
+    const { email, avatar, full_name }: IUser = req.body;
 
-  if (!email && !avatar && !full_name) {
-    return res
-      .status(500)
-      .json({ message: "Missing email or avatar or full_name" });
-  }
+    if (!email && !avatar && !full_name) {
+      return res
+        .status(500)
+        .json({ message: "Missing email or avatar or full_name" });
+    }
 
-  const user = await User.findOne({ email: email });
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const newUser = new User({
+        email,
+        avatar,
+        full_name,
+        role: ROLE.user,
+      });
 
-  if (!user) {
-    const newUser = new User({
-      email,
-      avatar,
-      full_name,
-      role: ROLE.user,
-    });
-
-    newUser.created_at = getNow();
-    newUser.created_by = email + " register gmail";
-    await newUser.save();
-  } else {
-    const contentToken = { _id: user.id, role: user.role };
-    const access_token = tokenGen(
-      contentToken,
-      parseInt(<string>process.env.EXPIRED_ACCESS_TOKEN)
-    );
-    const refresh_token = tokenGen(
-      contentToken,
-      parseInt(<string>process.env.EXPIRED_REFRESH_TOKEN)
-    );
-    user.access_token = access_token;
-    user.refresh_token = refresh_token;
-    await user.save();
-    return res.status(200).json({
-      message: "Login success",
-      access_token,
-      refresh_token,
-    });
+      newUser.created_at = getNow();
+      newUser.created_by = email + " register gmail";
+      await newUser.save();
+    } else {
+      const contentToken = { _id: user.id, role: user.role };
+      const access_token = tokenGen(
+        contentToken,
+        parseInt(<string>process.env.EXPIRED_ACCESS_TOKEN)
+      );
+      const refresh_token = tokenGen(
+        contentToken,
+        parseInt(<string>process.env.EXPIRED_REFRESH_TOKEN)
+      );
+      user.access_token = access_token;
+      user.refresh_token = refresh_token;
+      await user.save();
+      return res.status(200).json({
+        message: "Login success",
+        access_token,
+        refresh_token,
+      });
+    }
+  } catch (err) {
+    console.log(err);
   }
 };
 
